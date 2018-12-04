ce60ba225c3ce74a5c43ac9a5b724cd2360e4d12
Dmitriy <dim0k92sokol@gmail.com>
2018-12-04 13:35:06 +0200

    Test server listening

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 // index.js
+const http =require('http')
+const port=3000
+
+const requestHandler=(request, Response) => {
+    console.log(request.url)
+    Response.end('Hello node.js Server!')
+} 
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+    if (err){
+    return console.log('something bad happend', err)
+}
+console.log('server is listening on', port)
+})
+
 require('./app/index')
 const _ = require('lodash')
 _.assign({'a': 1}, {'b': 2}, {'c': 3});
